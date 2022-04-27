c47375b2668c943e00611a13b05d8677046d8481
Gabriel Løsnesløkken <glosneslokken@gmail.com>
2022-04-27 22:31:04 +0200

    Implement TetrisPieceBuilder functions so that it can create Classical TetrisPieces

--- src/Logic/TetrsPiece/TetrisPieceBuilder.cpp
+++ src/Logic/TetrsPiece/TetrisPieceBuilder.cpp
@@ -7,33 +7,108 @@
 namespace Tetris::Logic
 {
 
-
     State::TetrisPiece TetrisPieceBuilder::ClassicalPieces::buildPieceI()
     {
-        return State::TetrisPiece(State::TetrisSingleTile());
+        State::TetrisSingleTile tile{};
+        tile.setColor(sf::Color::Red);
+
+        State::TetrisPiece piece(tile);
+
+        piece.addTilePieceRelativeToCenter(-1, 0);
+        piece.addTilePieceRelativeToCenter( 0, 0);
+        piece.addTilePieceRelativeToCenter( 1, 0);
+        piece.addTilePieceRelativeToCenter( 2, 0);
+
+        return piece;
     }
 
-    State::TetrisPiece TetrisPieceBuilder::ClassicalPieces::buildPieceO() {
-        return State::TetrisPiece(State::TetrisSingleTile());
+    State::TetrisPiece TetrisPieceBuilder::ClassicalPieces::buildPieceO()
+    {
+        State::TetrisSingleTile tile{};
+        tile.setColor(sf::Color::Red);
+
+        State::TetrisPiece piece(tile);
+
+        piece.addTilePieceRelativeToCenter(-1, -1);
+        piece.addTilePieceRelativeToCenter( 0, -1);
+        piece.addTilePieceRelativeToCenter(-1,  0);
+        piece.addTilePieceRelativeToCenter( 0,  0);
+
+        return piece;
     }
 
-    State::TetrisPiece TetrisPieceBuilder::ClassicalPieces::buildPieceT() {
-        return State::TetrisPiece(State::TetrisSingleTile());
+    State::TetrisPiece TetrisPieceBuilder::ClassicalPieces::buildPieceT()
+    {
+        State::TetrisSingleTile tile{};
+        tile.setColor(sf::Color::Red);
+
+        State::TetrisPiece piece(tile);
+
+        piece.addTilePieceRelativeToCenter( 0, -1);
+        piece.addTilePieceRelativeToCenter(-1,  0);
+        piece.addTilePieceRelativeToCenter( 0,  0);
+        piece.addTilePieceRelativeToCenter( 1,  0);
+
+        return piece;
     }
 
-    State::TetrisPiece TetrisPieceBuilder::ClassicalPieces::buildPieceS() {
-        return State::TetrisPiece(State::TetrisSingleTile());
+    State::TetrisPiece TetrisPieceBuilder::ClassicalPieces::buildPieceS()
+    {
+        State::TetrisSingleTile tile{};
+        tile.setColor(sf::Color::Red);
+
+        State::TetrisPiece piece(tile);
+
+        piece.addTilePieceRelativeToCenter( 0,  1);
+        piece.addTilePieceRelativeToCenter( 1,  1);
+        piece.addTilePieceRelativeToCenter(-1,  0);
+        piece.addTilePieceRelativeToCenter( 0,  0);
+
+        return piece;
     }
 
-    State::TetrisPiece TetrisPieceBuilder::ClassicalPieces::buildPieceZ() {
-        return State::TetrisPiece(State::TetrisSingleTile());
+    State::TetrisPiece TetrisPieceBuilder::ClassicalPieces::buildPieceZ()
+    {
+        State::TetrisSingleTile tile{};
+        tile.setColor(sf::Color::Red);
+
+        State::TetrisPiece piece(tile);
+
+        piece.addTilePieceRelativeToCenter(-1, 1);
+        piece.addTilePieceRelativeToCenter( 0, 1);
+        piece.addTilePieceRelativeToCenter( 0, 0);
+        piece.addTilePieceRelativeToCenter( 1, 0);
+
+        return piece;
     }
 
-    State::TetrisPiece TetrisPieceBuilder::ClassicalPieces::buildPieceJ() {
-        return State::TetrisPiece(State::TetrisSingleTile());
+    State::TetrisPiece TetrisPieceBuilder::ClassicalPieces::buildPieceJ()
+    {
+        State::TetrisSingleTile tile{};
+        tile.setColor(sf::Color::Red);
+
+        State::TetrisPiece piece(tile);
+
+        piece.addTilePieceRelativeToCenter(-1, 1);
+        piece.addTilePieceRelativeToCenter(-1, 0);
+        piece.addTilePieceRelativeToCenter( 0, 0);
+        piece.addTilePieceRelativeToCenter( 0, 1);
+
+        return piece;
     }
 
-    State::TetrisPiece TetrisPieceBuilder::ClassicalPieces::buildPieceL() {
-        return State::TetrisPiece(State::TetrisSingleTile());
+    State::TetrisPiece TetrisPieceBuilder::ClassicalPieces::buildPieceL()
+    {
+        State::TetrisSingleTile tile{};
+        tile.setColor(sf::Color::Red);
+
+        State::TetrisPiece piece(tile);
+
+        piece.addTilePieceRelativeToCenter( 1, 1);
+        piece.addTilePieceRelativeToCenter(-1, 0);
+        piece.addTilePieceRelativeToCenter( 0, 0);
+        piece.addTilePieceRelativeToCenter( 0, 1);
+
+        return piece;
     }
 }
