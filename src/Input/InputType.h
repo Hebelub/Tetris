341723725fa7048a71b7cf94be804a8a6616bcc7
Gabriel Løsnesløkken <glosneslokken@gmail.com>
2022-04-27 14:02:17 +0200

    Add InputType interface (only virtual functions)

---
+++ src/Input/InputType.h
@@ -0,0 +1,28 @@
+//
+// Created by glosn on 4/27/2022.
+//
+
+#ifndef TETRISEXAM_INPUTTYPE_H
+#define TETRISEXAM_INPUTTYPE_H
+
+namespace Tetris::Input
+{
+    /// @brief An interface
+    class InputType
+    {
+        virtual bool turnRightIsPressed() = 0;
+
+        virtual bool turnLeftIsPressed() = 0;
+
+        virtual bool instantFallIsPressed() = 0;
+
+        virtual bool speedFallIsPressed() = 0;
+
+        virtual bool moveRightIsPressed() = 0;
+
+        virtual bool moveLeftIsPressed() = 0;
+    };
+
+} // Tetris::Input
+
+#endif //TETRISEXAM_INPUTTYPE_H
