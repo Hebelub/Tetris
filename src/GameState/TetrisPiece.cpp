5ed0526a81701e75de9c32f2a71e669020ff21c3
Gabriel Løsnesløkken <glosneslokken@gmail.com>
2022-04-27 21:31:38 +0200

    Implemented TetrisPiece

--- src/GameState/TetrisPiece.cpp
+++ src/GameState/TetrisPiece.cpp
@@ -6,8 +6,16 @@
 
 namespace Tetris::State
 {
-    void TetrisPiece()
+    /// @param tileTemplate the tile witch will be copied to create the tile
+    TetrisPiece::TetrisPiece(TetrisSingleTile const& tileTemplate)
+        : m_tileTemplate(tileTemplate)
     {
 
-    };
+    }
+
+    void TetrisPiece::addTilePieceRelativeToCenter(int xOffset, int yOffset)
+    {
+        m_tetrisPieces.push_back({xOffset, yOffset, m_tileTemplate});
+    }
+
 }
