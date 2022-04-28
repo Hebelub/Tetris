52b0bfdb4889aef3cc64395323d982514513f439
Gabriel Løsnesløkken <glosneslokken@gmail.com>
2022-04-28 11:46:16 +0200

    Add ActiveTetrisPiece class

---
+++ src/Logic/TetrsPiece/ActiveTetrisPiece.cpp
@@ -0,0 +1,44 @@
+//
+// Created by glosn on 4/28/2022.
+//
+
+#include "ActiveTetrisPiece.h"
+
+namespace Tetris::Logic
+{
+    ActiveTetrisPiece::ActiveTetrisPiece(State::GridState &grid)
+        : m_gridState(grid)
+    {
+
+    }
+
+    void ActiveTetrisPiece::fallOnce()
+    {
+
+    }
+
+    void ActiveTetrisPiece::fallToTheBottom()
+    {
+
+    }
+
+    void ActiveTetrisPiece::rotateRight()
+    {
+
+    }
+
+    void ActiveTetrisPiece::rotateLeft()
+    {
+
+    }
+
+    void ActiveTetrisPiece::getBottomCells()
+    {
+
+    }
+
+    void ActiveTetrisPiece::updatePosition()
+    {
+
+    }
+} // Tetris::Logic
---
+++ src/Logic/TetrsPiece/ActiveTetrisPiece.h
@@ -0,0 +1,42 @@
+//
+// Created by glosn on 4/28/2022.
+//
+
+#ifndef TETRISEXAM_ACTIVETETRISPIECE_H
+#define TETRISEXAM_ACTIVETETRISPIECE_H
+
+/// @brief Manages falling and rotation of piece. Also instantiation of tiles into the grid
+#include <SFML/System/Vector2.hpp>
+#include "../../GameState/TetrisTile.h"
+#include "../../GameState/GridState.h"
+
+namespace Tetris::Logic
+{
+    class ActiveTetrisPiece
+    {
+    public:
+        explicit ActiveTetrisPiece(State::GridState &grid);
+
+        void fallOnce();
+        void fallToTheBottom();
+
+        void rotateRight();
+        void rotateLeft();
+
+    private:
+        // A value between one and four
+        int m_rotation{};
+        State::TetrisTile m_currentTile;
+        sf::Vector2i m_currentPosition;
+        State::GridState &m_gridState;
+
+
+        void moveTo(sf::Vector2i newPosition);
+        void canMoveTo(sf::Vector2i position);
+        void getBottomCells();
+        void updatePosition();
+    };
+
+} // Tetris::Logic
+
+#endif //TETRISEXAM_ACTIVETETRISPIECE_H
