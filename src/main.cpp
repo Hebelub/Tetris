fdb42dc6caf7f1fe043416e24797f91a146a3961
Gabriel Løsnesløkken <gabriell@uia.no>
2024-01-04 14:13:43 +0100

    Automatically copy DLL-s to cmake-build-debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 #include "RunningGame/Application.h"
-#include "Input/PlayerInput.h"
-
-#include <memory>
-#include <random>
-
 
 int main()
 {
     Tetris::Application game;
     game.runGameLoop();
-    return 0;
 }
